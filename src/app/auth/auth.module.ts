1476013e97578cd07e40a007c91f31c938548c64
Juan Ortega <jr.ortega7@gmail.com>
2022-04-17 15:01:40 -0400

    Fin seccion 7

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { LoginComponent } from './login/login.component';
@@ -19,8 +20,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
-
+    RouterModule,
+    FormsModule,
   ]
 })
 export class AuthModule { }
